624e9c2559697b0f466fdf94e60c83c5220775e4
Travis Herrick <tthetoad@gmail.com>
2011-08-08 00:05:16 -0400

    Added comments to app_state.rb.

--- lib/test_internals/app_state.rb
+++ lib/test_internals/app_state.rb
@@ -1,3 +1,39 @@
+# This file contains the constant that is used to determine whether the
+# application is still running or not.
+
+#--
+################################################################################
+#                      Copyright (C) 2011 Travis Herrick                       #
+################################################################################
+#                                                                              #
+#                                 \v^V,^!v\^/                                  #
+#                                 ~%       %~                                  #
+#                                 {  _   _  }                                  #
+#                                 (  *   -  )                                  #
+#                                 |    /    |                                  #
+#                                  \   _,  /                                   #
+#                                   \__.__/                                    #
+#                                                                              #
+################################################################################
+# This program is free software: you can redistribute it                       #
+# and/or modify it under the terms of the GNU General Public License           #
+# as published by the Free Software Foundation,                                #
+# either version 3 of the License, or (at your option) any later version.      #
+#                                                                              #
+# Commercial licensing may be available for a fee under a different license.   #
+################################################################################
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY;                                                    #
+# without even the implied warranty of MERCHANTABILITY                         #
+# or FITNESS FOR A PARTICULAR PURPOSE.                                         #
+# See the GNU General Public License for more details.                         #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+#++
+
 module TestInternals
+  # This constant is used to track whether the application is still running.
   AppState = AppMode.new(:alive, [:alive, :dead])
 end
